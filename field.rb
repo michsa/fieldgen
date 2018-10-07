2b53d4c1031f66dd411e6cae2fe0a59c455dea8a
Michelle Saad <git@michsa.me>
2018-10-07 16:32:14 -0400

    add flags for r2 and server

--- field.rb
+++ field.rb
@@ -1,17 +1,47 @@
+require 'optparse'
+
+dir = 'classic'
+use_server = false
+server_format = 0
+
+OptionParser.new do |parser|
+  parser.on('-s', '--server[n]') do |arg|
+    use_server = true
+    if arg == 'n' then server_format = 1 end
+  end
+  parser.on('--server-name') do
+    use_server = true
+    server_format = 1
+  end
+  parser.on('-r2') { dir = 'r2' }
+end.parse!
+
 count = [ARGV[0].to_i, 1].max
 arr = Array.new
 
-for x in 1..4 do
-  arr[x-1] = Array.new
-  File.open("#{x-1}.txt", "r").each_line do |line|
-    arr[x-1].push(line.strip)
+def read_file(path)
+  items = Array.new
+  File.open(path, "r").each_line do |line|
+    items.push(line.strip)
   end
+  items
+end
+
+if use_server
+  servers = read_file('0.txt').map do |line|
+    line.split(" ")[server_format]
+  end
+  arr.push servers
+end
+
+for x in 1..3 do
+  arr.push read_file("#{dir}/#{x}.txt")
 end
 
 for i in 1..count do
-  str = ""
+  str = ''
   for word in arr do
-    str += word.sample + " "
+    str += word.sample + ' '
   end
   puts str
 end
